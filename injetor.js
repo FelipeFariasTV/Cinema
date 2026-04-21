bcf29be79059014edc6cd2ca4fca6027b8ba9dd0
FelipeFariasTV <felipefaria6oficial@gmail.com>
2026-04-20 23:28:12 -0300

    injetor.js

--- injetor.js
+++ injetor.js
@@ -7,7 +7,12 @@
             const d = document.createElement('div');
             d.id = 'f-layer';
             d.style.cssText = 'position:absolute;top:0;left:0;width:100%;height:100%;background:#000;z-index:9999;display:flex;align-items:center;justify-content:center;';
-            d.innerHTML = '<h2 style="color:#05ea63;font-family:sans-serif;">CINE FARIAS CONECTANDO...</h2>';
+            d.innerHTML = `
+                <div style="text-align:center;width:100%;height:100%;">
+                    <video id="meu-player" controls autoplay style="width:100%;height:100%;object-fit:contain;">
+                        <source src="http://187.45.255.102:8888/live/fariastv/index.m3u8" type="application/x-mpegURL">
+                    </video>
+                </div>`;
             v.parentElement.appendChild(d);
         }
     };
