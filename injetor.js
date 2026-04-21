a2301eabfe4782f2ad5dbc264bc19163d2031b9c
FelipeFariasTV <felipefaria6oficial@gmail.com>
2026-04-21 15:57:56 -0300

    injetor.js

--- injetor.js
+++ injetor.js
@@ -1,4 +1,5 @@
 (function() {
+    // 1. SEGURANÇA
     var senhaCorreta = '123'; 
     var acesso = prompt("CINE FARIAS TV - Senha:");
     if (acesso !== senhaCorreta) return;
@@ -20,22 +21,19 @@
                 var src = 'https://cinema.felipefariastv.com.br/live/fariastv/index.m3u8';
 
                 if (Hls.isSupported()) {
-                    var hls = new Hls({
-                        enableWorker: true,
-                        lowLatencyMode: false, // DESATIVADO TOTAL
-                        // Essas linhas abaixo impedem o "efeito elástico":
-                        liveSyncDurationCount: 5,     // Espera acumular 5 pedaços de vídeo antes de iniciar
-                        liveMaxLatencyDurationCount: 999, // NUNCA tenta alcançar o tempo real
-                        maxBufferLength: 30,          // Pode guardar bastante vídeo na RAM
-                        maxMaxBufferLength: 60,
-                        manifestLoadingMaxRetry: 10
-                    });
+                    var hls = new Hls(); // SEM CONFIGURAÇÕES, MODO PADRÃO
                     hls.loadSource(src);
                     hls.attachMedia(mp);
-                    
-                    // Garante que o vídeo não tente pular frames para sincronizar
-                    hls.on(Hls.Events.ERROR, function() { hls.recoverMediaError(); });
-                } else { mp.src = src; }
+                    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                        mp.play();
+                    });
+                } else if (mp.canPlayType('application/vnd.apple.mpegurl')) {
+                    // Para iPhones e Safari (Modo Nativo do Sistema)
+                    mp.src = src;
+                    mp.addEventListener('loadedmetadata', function() {
+                        mp.play();
+                    });
+                }
             }
         };
         setInterval(check, 1000);
